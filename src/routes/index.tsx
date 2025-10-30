4080e4f0edbf6d659f596816965df8eeb1fef929
nir2002 <nir.parisian@gmail.com>
2025-10-30 19:44:19 +0200

    fix: import file name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Suspense } from 'react'
-import { Spinner } from '~/components/ui/spinner'
+import { Spinner } from '~/components/ui/Spinner'
 import { DashboardPage } from '~/features/speakers/pages/DashboardPage'
 
-export const Route = createFileRoute('/')({   component: () => 
-      <Suspense fallback={<Spinner size="lg" />}>
-        <DashboardPage />
-      </Suspense>
- ,         
-  errorComponent: () =>  <p>Error</p>, })
+export const Route = createFileRoute('/')({
+  component: () => (
+    <Suspense fallback={<Spinner size="lg" />}>
+      <DashboardPage />
+    </Suspense>
+  ),
+  errorComponent: () => <p>Error</p>,
+})
