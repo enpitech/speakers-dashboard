5342c6cf1376bd988e570cbd8595cec3d4aeaa2b
yarinmzrc <yarinmzrc@gmail.com>
2025-11-13 17:24:12 +0200

    add types for option and topics

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,17 +4,23 @@ import type {
   SocialPlatform as PrismaSocialPlatform,
   SocialPlatform as PrismaSocialPlatformEnum,
   Speaker as PrismaSpeaker,
-} from '@prisma/client';
-  
+} from '@prisma/client'
+
+export type Option = {
+  value: string
+  label: string
+}
+
 export type Speaker = PrismaSpeaker & {
-  socialLinks: SocialLink[];
-};
+  socialLinks: SocialLink[]
+  topics: Option[]
+}
 
-export type SocialLink = PrismaSocialLink;
+export type SocialLink = PrismaSocialLink
 
-export type SocialPlatform = PrismaSocialPlatform;
+export type SocialPlatform = PrismaSocialPlatform
 
-export const SocialPlatformEnum:Record<PrismaSocialPlatformEnum, string> = {
+export const SocialPlatformEnum: Record<PrismaSocialPlatformEnum, string> = {
   LINKEDIN: 'LINKEDIN',
   TWITTER: 'TWITTER',
   INSTAGRAM: 'INSTAGRAM',
@@ -24,4 +30,4 @@ export const SocialPlatformEnum:Record<PrismaSocialPlatformEnum, string> = {
   TIKTOK: 'TIKTOK',
   SPOTIFY: 'SPOTIFY',
   DISCORD: 'DISCORD',
-} 
+}
