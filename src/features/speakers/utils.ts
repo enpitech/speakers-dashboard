fa5b8c2ae42dd0f7968af2c7d4d2671749a1c27b
yarinmzrc <yarinmzrc@gmail.com>
2025-11-20 15:59:48 +0200

    select only id + title

--- src/features/speakers/utils.ts
+++ src/features/speakers/utils.ts
@@ -1,17 +1,16 @@
 import type { Topic } from '@prisma/client'
 
-export function mapTopicsToValueLabel(
-  topics: Omit<Topic, 'createdAt' | 'updatedAt'>[],
+export function mapTopicToValueLabel(
+  topic: Omit<Topic, 'createdAt' | 'updatedAt'>,
 ) {
-  return topics.map((topic) => ({
-    value: topic.id,
-    label: topic.title,
-  }))
-}
-
-export function mapTopicToValueLabel(topic: Topic) {
   return {
     value: topic.id,
     label: topic.title,
   }
+}
+
+export function mapTopicsToValueLabel(
+  topics: Omit<Topic, 'createdAt' | 'updatedAt'>[],
+) {
+  return topics.map(mapTopicToValueLabel)
 }
