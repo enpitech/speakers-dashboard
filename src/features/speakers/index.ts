d167b0a9496896beed458c6cdbb66057cf299107
shmuel cohen <shmue.c@orchid.security>
2025-11-23 09:04:28 +0200

    feat: add loading animation and new speaker components to DashboardPage

--- src/features/speakers/index.ts
+++ src/features/speakers/index.ts
@@ -1,3 +1,5 @@
 // Export speaker components for use by other parts of the application
 
 export { SpeakersTable } from './components/SpeakersTable'
+export { SpeakersGrid } from './components/SpeakersGrid'
+export { SpeakerCard } from './components/SpeakerCard'
